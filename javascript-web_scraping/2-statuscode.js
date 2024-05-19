1fe5c60949dd478c2f4600841598619d51c18c6e
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-05-19 22:42:49 +0400

    Status code

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* eslint-disable */
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (error, response) => {
+	if (error) throw error;
+  	console.log(`code: ${response.statusCode}`);
+});
