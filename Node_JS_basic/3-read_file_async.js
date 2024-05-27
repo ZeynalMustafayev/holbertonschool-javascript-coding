176508cd660814e84bc11484681996977842d504
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-05-27 18:43:29 +0400

    add async and await

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+async function countStudents(path) {
+  try {
+    const data = await fs.readFileSync(path, 'UTF-8');
+    const lines = data.trim().split('\n');
+
+    let countCs = 0;
+    let countSwe = 0;
+    const csNames = [];
+    const sweNames = [];
+
+    lines.forEach((line) => {
+      const fields = line.split(',');
+
+      if (fields.length === 4 && fields[0] !== '' && fields[1] !== '' && fields[2] !== '' && fields[3] !== '') {
+        if (fields[3].trim() === 'CS') {
+          countCs += 1;
+          csNames.push(fields[0]);
+        } else if (fields[3].trim() === 'SWE') {
+          countSwe += 1;
+          sweNames.push(fields[0]);
+        }
+      }
+    });
+
+    console.log(`Number of students: ${lines.length - 1}`);
+    console.log(`Number of students in CS: ${countCs}. List: ${csNames.join(', ')}`);
+    console.log(`Number of students in SWE: ${countSwe}. List: ${sweNames.join(', ')}`);
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+module.exports = countStudents;
