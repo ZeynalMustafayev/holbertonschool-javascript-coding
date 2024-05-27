963a875b9a5d557c1e090ab7c1bbf51c4225fa88
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-05-27 18:08:32 +0400

    Reading a file synchronously with Node JS

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,10 +1,33 @@
-function countStudents() {
-    const fs = require("fs");
-    const data = fs.ReadStream("database.csv", "UTF-8");
-    if (!(data)) {
+const fs = require("fs");
+function countStudents(path) {
+    const data = fs.readFileSync(path, "UTF-8");
+    if (data.length === 0) {
         throw new Error("Cannot load the database")
     }
-    console.log(`Number of students: ${data.length}`)
-    console.log(`Number of students in ${data.field}: 6. List: ${data.firstname}`)
+    const lines = data.trim().split("\n");
+
+    let countCs = 0
+    let countSwe = 0
+    let csNames = []
+    let sweNames = []
+
+    lines.forEach(line => {
+        const fields = line.split(",")
+
+        if (fields.length === 4 && fields[0] !== "", fields[1] !== "", fields[2] !== "", fields[3] !== "") {
+            if (fields[3].trim() === "CS") {
+                countCs += 1;
+                csNames.push(fields[0])
+            }
+            else if (fields[3].trim() === "SWE") {
+                countSwe += 1;
+                sweNames.push(fields[0])
+            }
+        }
+    })
+
+    console.log(`Number of students: ${lines.length - 1}`)
+    console.log(`Number of students in CS: ${countCs}. List: ${csNames.join(", ")}`)
+    console.log(`Number of students in SWE: ${countSwe}. List: ${sweNames.join(", ")}`)
 }
 module.exports = countStudents;
