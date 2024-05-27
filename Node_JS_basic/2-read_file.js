05dfc0ad9183bfddc7acd7bcf4a3bf1baed93f30
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-05-27 18:24:23 +0400

    sometimes I'm stupid

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -2,29 +2,24 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, 'utf8');
+    const data = fs.readFileSync(path, 'UTF-8');
     const lines = data.trim().split('\n');
-
-    if (lines.length < 2) {
-      throw new Error('Cannot load the database');
-    }
 
     let countCs = 0;
     let countSwe = 0;
     const csNames = [];
     const sweNames = [];
 
-    lines.forEach((line, index) => {
-      if (index === 0) return;
+    lines.forEach((line) => {
       const fields = line.split(',');
 
       if (fields.length === 4 && fields[0] !== '' && fields[1] !== '' && fields[2] !== '' && fields[3] !== '') {
         if (fields[3].trim() === 'CS') {
           countCs += 1;
-          csNames.push(fields[0].trim());
+          csNames.push(fields[0]);
         } else if (fields[3].trim() === 'SWE') {
           countSwe += 1;
-          sweNames.push(fields[0].trim());
+          sweNames.push(fields[0]);
         }
       }
     });
@@ -36,5 +31,4 @@ function countStudents(path) {
     throw new Error('Cannot load the database');
   }
 }
-
 module.exports = countStudents;
