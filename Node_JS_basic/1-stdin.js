9a473741929e2fbb1122f72a62aec58f98125c4b
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-05-27 16:30:44 +0400

    fix error

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ const readline = require('readline');
 
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
-console.log('Welcome to Holberton School, what is your name?');
+rl.write('Welcome to Holberton School, what is your name?\n');
 rl.on('line', (input) => {
   console.log(`Your name is: ${input}`);
   rl.close();
