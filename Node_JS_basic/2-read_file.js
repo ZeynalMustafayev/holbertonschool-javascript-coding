af3d3d607d549a330dad29a7b90fabbb56ea5b20
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-05-27 18:20:28 +0400

    fix eslint error

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -2,24 +2,29 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, 'UTF-8');
+    const data = fs.readFileSync(path, 'utf8');
     const lines = data.trim().split('\n');
+
+    if (lines.length < 2) {
+      throw new Error('Cannot load the database');
+    }
 
     let countCs = 0;
     let countSwe = 0;
     const csNames = [];
     const sweNames = [];
 
-    lines.forEach((line) => {
+    lines.forEach((line, index) => {
+      if (index === 0) return;
       const fields = line.split(',');
 
-      if (fields.length === 4 && fields[0] !== '', fields[1] !== '', fields[2] !== '', fields[3] !== '') {
+      if (fields.length === 4 && fields[0] !== '' && fields[1] !== '' && fields[2] !== '' && fields[3] !== '') {
         if (fields[3].trim() === 'CS') {
           countCs += 1;
-          csNames.push(fields[0]);
+          csNames.push(fields[0].trim());
         } else if (fields[3].trim() === 'SWE') {
           countSwe += 1;
-          sweNames.push(fields[0]);
+          sweNames.push(fields[0].trim());
         }
       }
     });
@@ -31,4 +36,5 @@ function countStudents(path) {
     throw new Error('Cannot load the database');
   }
 }
+
 module.exports = countStudents;
