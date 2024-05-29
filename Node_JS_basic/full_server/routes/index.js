e9d89d3aae0f1aef787b5b9706ebbf94af281316
ZeynalMustafayev <zeynal.mustafayev.099@gmail.com>
2024-05-29 19:40:35 +0400

    routes

--- Node_JS_basic/full_server/routes/index.js
+++ Node_JS_basic/full_server/routes/index.js
@@ -1,11 +1,13 @@
-import express from 'express';
+/* eslint-disable */
 import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
 
-const router = express.Router();
 
-router.get('/', AppController.getHomepage);
-router.get('/students', StudentsController.getAllStudents);
-router.get('/students/:major', StudentsController.getAllStudentsByMajor);
+const mapRoutes = (app) => {
+  app.get('/', AppController.getHomepage);
+  app.get('/students', StudentsController.getAllStudents);
+  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
+};
 
-export default router;
+export default mapRoutes;
+module.exports = mapRoutes;
